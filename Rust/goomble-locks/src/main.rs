5c5e40059fc14e507433da43c529a39e7553d52b
Nic McPhee <mcphee@morris.umn.edu>
2022-03-09 08:44:54 -0600

    Set up Rust project

---
+++ Rust/goomble-locks/src/main.rs
@@ -0,0 +1,12 @@
+struct Goombler {
+    balance: u32
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    //     private static final int MAX_BALANCE = 100;
+    //         final int numPresses = numGoomblers * MAX_BALANCE;
+    NUM_GOOMBLERS = 12;
+    goomblers: Goombler[NUM_GOOMBLERS];
+}
