0c6695d92d6d01cd315dfa99793eebd930c5763a
Nic McPhee <mcphee@morris.umn.edu>
2022-03-24 22:44:56 -0500

    Get the Rust version with channels working
    This gets the Rust version with channels working!
    
    I must confess I still only half understand ownership and mutability and references in Rust, and there was a lot of flailing and add/removing `&` and `mut` here and there to get this to work, but it does work, and it's actually 25-30% faster than any of the other versions. I suspect this is because there's essentially no locking here. In general things only block if they are attempting to read from an empty channel, and that probably doesn't happen often until the end when all the channels close up. Threads will get pulled off the CPU here and there to make room for other threads, but probably less than with all the lock checking in the other approaches.

--- Rust/goomble-channels/src/main.rs
+++ Rust/goomble-channels/src/main.rs
@@ -1,24 +1,11 @@
-/*
- * I SHOULD TOTALLY REPLACE ALL THIS WITH THE CROSSBEAM CRATE
- * AS IT LOOKS LIKE IT WILL BE MUCH EASIER TO WORK WITH IN THE
- * WAY THAT I'M THINKING OF THINGS.
- * 
- * https://docs.rs/crossbeam-channel/0.5.4/crossbeam_channel/
- */
-
 use crossbeam_channel::unbounded;
+use crossbeam_channel::Sender;
 use crossbeam_utils::thread;
-// use std::sync::mpsc;
-// use std::sync::mpsc::Sender;
-// use std::sync::mpsc::Receiver;
-// use std::sync::Arc;
-// use std::thread;
-// use std::time::Duration;
+use std::time::{Instant, Duration};
 use std::hash::Hash;
 use std::hash::Hasher;
 use rand::Rng;
 use rand::prelude::SliceRandom;
-use threadpool::ThreadPool;
 use uuid::Uuid;
 
 struct Goombler {
@@ -42,7 +29,6 @@ impl Hash for Goombler {
 static NUM_GOOMBLERS: u32 = 12;
 static MAX_BALANCE: u32 = 100;
 static NUM_PRESSES: u32 = NUM_GOOMBLERS * MAX_BALANCE;
-static NUM_THREADS: usize = 10;
 
 fn init_goomblers(num_goomblers: u32) -> (Vec<Goombler>, u32) {
     let mut rng = rand::thread_rng();
@@ -62,76 +48,66 @@ fn init_goomblers(num_goomblers: u32) -> (Vec<Goombler>, u32) {
     return (goomblers, initial_goomblers_total_balance);
 }
 
-fn lucky(goombler: &Goombler) {
-    println!("Pressing a button for {}.", goombler.id);
-    let mut balance = goombler.balance;
-    if balance > 0 {
-        balance -= 1;
-        // // Frees up the lock on balance since we don't need that anymore.
-        // drop(balance);
-        // let mut goomble_balance = goomble_balance.lock().unwrap();
-        // *goomble_balance += 1;
+fn lucky(goombler: &mut Goombler, goomble_send: &Sender<u32>) {
+    let mut rng = rand::thread_rng();
+    // println!("Pressing a button for {}.", goombler.id);
+    if goombler.balance > 0 {
+        std::thread::sleep(Duration::from_millis(rng.gen::<u64>() % 60));
+        goombler.balance -= 1;
+        goomble_send.send(1).unwrap();
     }
 }
 
 fn main() {
-    let (goomblers, initial_goomblers_total_balance) = init_goomblers(NUM_GOOMBLERS);
+    let (mut goomblers, initial_goomblers_total_balance) = init_goomblers(NUM_GOOMBLERS);
+    println!("Initial total goomblers' balance was {}", initial_goomblers_total_balance);
+    let (goomble_send, goomble_receive) = unbounded::<u32>();
 
-    // let mut channel_map = HashMap::new();
-    // for goombler in goomblers.iter() {
-    //     let (s, r) = unbounded::<String>();
-    //     channel_map.insert(goombler, (s, r));
-    // }
+    let start = Instant::now();
 
     // Building on https://docs.rs/crossbeam/0.8.1/crossbeam/thread/index.html
     let mut senders = Vec::new();
     thread::scope(|sc| {
-        for goombler in &goomblers {
+        for goombler in &mut goomblers {
             let (s, r) = unbounded::<String>();
             senders.push(s);
+            let gs = goomble_send.clone();
             sc.spawn(move |_| {
                 for _ in r {
-                    lucky(&goombler);
+                    lucky(goombler, &gs);
                 }
+                drop(gs);
             });
+        }
+        drop(goomble_send);
+
+        sc.spawn(move |_| {
+            let mut goomble_balance = 0;
+            for change in goomble_receive {
+                goomble_balance += change;
+                // println!("Updating Goomble balance to {}.", goomble_balance);
+            }
+            println!("Final Goomble balance was {}.", goomble_balance);
+        });
+
+        for _ in 0..NUM_PRESSES {
+            let s = senders.choose(&mut rand::thread_rng()).unwrap();
+            s.send(String::from("push")).unwrap();
+        }
+
+        for s in senders {
+            drop(s);
         }
     }).unwrap();
 
-    for _ in 0..NUM_PRESSES {
-        // let goomblers = Arc::clone(&arc_goomblers);
-        // let goomble_balance = Arc::clone(&goomble_balance);
-        // let goombler = goomblers.choose(&mut rand::thread_rng()).unwrap();
-        // let (s, r) = channel_map.get(goombler).unwrap();
-        let s = senders.choose(&mut rand::thread_rng()).unwrap();
-        s.send(String::from("push")).unwrap();
-        // let s = s.clone();
-        // thread_pool.execute(move|| {
-        //     tx.send(String::from("push")).unwrap();
-        //     // lucky(goombler);
-        //     // lucky(goombler, goomble_balance);
-        // });
-    }
+    let duration = start.elapsed();
 
+    let mut final_goombler_balance = 0;
     for goombler in &goomblers {
         println!("Goombler final balance = {}", goombler.balance);
+        final_goombler_balance += goombler.balance;
     }
+    println!("\tTotal final Goombler balance = {}", final_goombler_balance);
 
-    // let tx1 = tx.clone();
-    // thread::spawn(move || {
-    //     let vals = vec![
-    //         String::from("hi"),
-    //         String::from("from"),
-    //         String::from("the"),
-    //         String::from("thread"),
-    //     ];
-
-    //     for val in vals {
-    //         tx.send(val).unwrap();
-    //         thread::sleep(Duration::from_secs(1));
-    //     }
-    // });
-
-    // for received in rx {
-    //     println!("Got: {}", received);
-    // }
+    println!("Total time was {:?}.", duration);
 }
